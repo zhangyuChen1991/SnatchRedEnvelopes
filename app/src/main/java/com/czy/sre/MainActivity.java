344c3b7175acea95da0b21fc50415ae67fce6600
zhangyuChen1991 <chenzhangyu1003@foxmail.com>
2017-01-11 10:46:13 +0800

    2017.01.11  UI调整

--- app/src/main/java/com/czy/sre/MainActivity.java
+++ app/src/main/java/com/czy/sre/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent = new Intent(this, SREService.class);
         initResources();
         initView();
+        updateServiceStatus();
 
     }
 
